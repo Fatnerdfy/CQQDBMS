4c1b6aa183a8cecf6c6c0423c5bcb0655562420f
Fatnerd <quanfy98@gmail.com>
2017-09-11 11:39:14 +0800

    feat : add reload block from disk
    Reload a block from disk which is not full and write new data to this block,  then write back to disk.

--- DBMSM1/DBMSM1/main.cpp
+++ DBMSM1/DBMSM1/main.cpp
@@ -25,8 +25,6 @@ struct header_info {
     int size;
 };
 
-// when the block isn't full write back to disk then reload to add data
-
 int disk_size = 100; // changeable
 int memory_size = 10000; // changeable
 
@@ -34,7 +32,7 @@ int block_no = 0; // No. of the memory block
 int disk_head_address = 0; // when write data to disk
 
 int counter = 0; // count the num of record(data)
-const int counter_length = 5; // save block head infomation(counter)
+const int counter_length = 20; // save block head infomation(counter)
 
 int Block_Size = OS_Block_Size * 1;
 int header_address = counter_length;
@@ -44,12 +42,14 @@ const int header_info_length = 15;
 char* allocate_memory(int size);
 struct header_info get_header_info(char* p);
 bool write_data(char* p, int block_no, char* data  = "test string");
-bool write_disk(FILE* fd, char* p);
+bool write_disk(FILE* fd, char* p, int block_num);
 FILE* allocate_disk();
 void test();
+void test1();
+void test2();
 
 int main(int argc, const char * argv[]) {
-    test();
+    test1();
     return 0;
 }
 
@@ -80,6 +80,9 @@ struct header_info get_header_info(char* p) {
 
 bool write_data(char* p, int block_no, char* data) {
     if(current_address - header_address < Block_Size * Empty_Space_ratio) {
+        char count[counter_length+1];
+        sprintf(count, "%d\t%d\t%d\t%d\t", counter, 1, header_address, current_address); // 1 is this block full
+        strncpy(p+block_no*Block_Size, count, strlen(count));
         printf("Achieve the limitation of empty space\n");
         return 0;
     } else {
@@ -95,22 +98,23 @@ bool write_data(char* p, int block_no, char* data) {
         counter += 1;
         
         char count[counter_length+1];
-        sprintf(count, "%d", counter);
+        // 0 is this block not full
+        sprintf(count, "%d\t%d\t%d\t%d\t", counter, 0, header_address, current_address);
         strncpy(p+block_no*Block_Size, count, strlen(count));
     }
     return 1;
 }
 
-bool write_disk(FILE* fd, char* p) {
+bool write_disk(FILE* fd, char* p, int block_num) {
     // allocate disk space
     if(fd==NULL) {
         printf("open file fail\n");
         return 0;
     } else {
-        if(disk_head_address+Block_Size*block_no+1 < disk_size*1024) {
+        if(disk_head_address+Block_Size*block_num < disk_size*1024) {
             fseek(fd, disk_head_address, SEEK_SET);
-            fwrite(p, Block_Size, block_no+1, fd);
-            disk_head_address += Block_Size;
+            fwrite(p, Block_Size, block_num, fd);
+            disk_head_address += Block_Size*block_num;
             fseek(fd, disk_size*1024, SEEK_SET);
             fprintf(fd,"\t");
         } else {
@@ -141,15 +145,26 @@ void set_param() {
     current_address = Block_Size - 2;
 }
 
-void write_block_head(char* p, int block_no) {
+void reload_block(FILE* fd, char* p) {
+    fseek(fd, 0, SEEK_SET);
+    fread(p, Block_Size, 2, fd);
+    char num[10];
+    int datanum = atoi(p+Block_Size);
+    sprintf(num, "%d", datanum);
+    header_address = atoi(p+Block_Size+strlen(num)+3);
+    int header_address_len = strlen(num);
+    memset(num, '\0', 10);
+    sprintf(num, "%d", header_address);
+    current_address = atoi(p+Block_Size+header_address_len+strlen(num)+4);
     
+    disk_head_address -= Block_Size;
 }
 
 void test() {
     FILE* fd = allocate_disk();
     char* p = allocate_memory(memory_size);
     if(p==NULL) {
-        printf("allocate memory failed\n");
+        return ;
     } else {
         // write data to block 0
         while(true) {
@@ -166,7 +181,49 @@ void test() {
             }
         }
         puts(p);
-        write_disk(fd, p);
+        write_disk(fd, p, block_no+1);
+    }
+    fclose(fd);
+}
+
+// write half of the block and write this block to disk
+// then reload this block from disk and add new data
+void test1() {
+    FILE* fd = allocate_disk();
+    char* p = allocate_memory(memory_size);
+    if(p==NULL) {
+        printf("allocate memory failed\n");
+    } else {
+        // write data to block 0
+        while(true) {
+            if(!write_data(p, block_no)) {
+                break;
+            }
+        }
+        set_param();
+        *(p+Block_Size*block_no-1) = '#';
+        // write data to block 1 & write half of the block
+        for (int i = 0; i < 50; i++) {
+            write_data(p, block_no, "hello world");
+        }
+        puts(p);
+        write_disk(fd, p, block_no+1);
+        /* test if p is clear
+        printf("\n\n==========================================\n\n");
+        memset(p, '-', Block_Size*2);
+        puts(p);        
+        */
+        printf("\n\n==========================================\n\n");
+        reload_block(fd, p);
+        //puts(p); // test reload success!
+        //printf("\n\n%d %d\n\n", header_address, current_address);
+        while (true) {
+            if (!write_data(p, 1, "half block test")) {
+                break;
+            }
+        }
+        puts(p);
+        write_disk(fd, p+Block_Size, 1);
     }
     fclose(fd);
 }
